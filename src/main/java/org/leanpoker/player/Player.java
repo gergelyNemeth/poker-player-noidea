0b48a447f5eed5429904f267eea43969c2b5ea96
Gergely Németh <ngerle@gmail.com>
2017-11-20 12:19:08 +0100

    Raise if highcard

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,38 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
+import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        JsonObject jobj = request.getAsJsonObject();
+        int pot = jobj.get("pot").getAsInt();
+        int bet = 0;
+
+        JsonArray players = jobj.getAsJsonArray("players");
+        for (JsonElement player : players) {
+            JsonObject playerObj = player.getAsJsonObject();
+            String playerName = playerObj.get("name").getAsString();
+            if (playerName.equals("noIDEa")) {
+                JsonArray cards = playerObj.getAsJsonArray("hole_cards");
+                int counter = 0;
+                for (JsonElement card : cards) {
+                    JsonObject cardObj = card.getAsJsonObject();
+                    int rank = cardObj.get("rank").getAsInt();
+                    if (rank > 10) {
+                        counter++;
+                    }
+                }
+                if (counter == 2) {
+                    bet = (int) Math.ceil(pot * 0.5);                }
+            }
+        }
+
+        return bet;
     }
 
     public static void showdown(JsonElement game) {
